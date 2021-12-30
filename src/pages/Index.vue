8c8090940d166a67741b41822c72c1c30cbfd419
fbiakbr <febiakbar19@gmail.com>
2021-12-30 22:05:25 +0700

    Adding Something

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -19,9 +19,9 @@
 
       <q-card-section v-if="todoList.length > 0">
         <div class="row justify-center" v-for="(item, index) in todoList" :key="index">
-          <div class="col-2 q-py-sm">{{ index + 1 }}</div>
+          <div class="col-2 q-py-sm flex flex-center">{{ index + 1 }}</div>
           <div class="col-8 q-py-sm">{{ item.value }}</div>
-          <div class="col-auto q-py-sm-md">
+          <div class="col-2 q-py-sm-md">
             <q-btn round dense flat icon="check" color="green" v-if="!item.done" @click="doneTodo(index)" />
             <q-btn round dense flat icon="restore" color="primary" v-if="item.done" @click="backtodoneTodo(index)" />
             <q-btn round dense flat icon="delete" color="red" v-if="item.done" @click="removeItem(index)" />
@@ -46,7 +46,7 @@
         <div class="row justify-center">
           <div class="col-12">
             <div class="text-center">
-              <p>Made with ❤️ in Pekalongan.</p>
+              <p>Made with ❤️ by Febiadi Wisnu Akbar.</p>
               <q-btn
                 size="10px"
                 color="primary"
@@ -71,7 +71,22 @@ export default defineComponent({
     return {
       todoTask: '',
       todoList: [
-        
+        {
+          value: 'Eat',
+          done: false,
+        },
+        {
+          value: 'Sleep',
+          done: false,
+        },
+        {
+          value: 'Code',
+          done: true,
+        },
+        {
+          value: 'Watching Anime',
+          done: false,
+        },
       ]
     }
   },
@@ -91,8 +106,9 @@ export default defineComponent({
       this.todoList[index].done = false;
     },
     removeItem(index) {
-      this.todoList.splice(index, 1);
-
+      if (confirm('Are you sure want to remove this item?')) {
+        this.todoList.splice(index, 1);
+      }
     }
   }
 })
