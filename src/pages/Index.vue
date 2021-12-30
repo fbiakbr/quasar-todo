6f248bd1b97f6fb51f5c4355d0edf961b3af9888
fbiakbr <febiakbar19@gmail.com>
2021-12-30 12:21:24 +0700

    To-Do List

---
+++ src/pages/Index.vue
@@ -0,0 +1,84 @@
+<template>
+  <q-page class="flex flex-center q-px-md">
+    <q-card class="my-card" style="width: 500px">
+      <q-card-section class="bg-primary">
+        <div class="text-h6 text-white">To-Do List</div>
+      </q-card-section>
+
+      <q-card-section>
+        <div class="row justify-center">
+          <div class="col-12 q-pb-sm">
+            <q-input v-model="todoTask" label="Input Task">
+              <template v-slot:after>
+                <q-btn round dense flat icon="send" @click="addItem" />
+              </template>
+            </q-input>
+          </div>
+        </div>
+      </q-card-section>
+
+      <q-card-section v-if="todoList.length > 0">
+        <div class="row justify-center" v-for="(item, index) in todoList" :key="index">
+          <div class="col-2 q-py-sm">{{ index + 1 }}</div>
+          <div class="col-8 q-py-sm">{{ item.value }}</div>
+          <div class="col-auto q-py-sm-md">
+            <q-btn round dense flat icon="done" @click="removeItem(index)" />
+          </div>
+        </div>
+      </q-card-section>
+
+      <q-card-section v-else>
+        <div class="row justify-center">
+          <div class="col-12">
+            <div class="text-center">
+              <q-icon name="info" />
+              <p>No items</p>
+            </div>
+          </div>
+        </div>
+      </q-card-section>
+
+      <q-separator />
+
+      <q-card-section>
+        <div class="row justify-center">
+          <div class="col-12">
+            <div class="text-center">
+              <p>Made with ❤️ in Pekalongan.</p>
+              <q-btn
+                size="10px"
+                color="primary"
+                class="q-btn--rounded"
+                href="https://www.textnice.tech"
+                target="_blank"
+              >TextNice</q-btn>
+            </div>
+          </div>
+        </div>
+      </q-card-section>
+    </q-card>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'PageIndex',
+  data() {
+    return {
+      todoTask: '',
+      todoList: []
+    }
+  },
+  methods: {
+    addItem() {
+      this.todoList.push({ value: this.todoTask });
+      this.todoTask = '';
+    },
+    removeItem(index) {
+      this.todoList.splice(index, 1);
+    }
+  }
+})
+</script>
